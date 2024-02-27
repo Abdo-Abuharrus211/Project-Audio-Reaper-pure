0e6d3b35afd700a42c9678b08b0b6fbb942396e9
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2024-02-27 14:49:42 -0800

    comment out certain functions

--- src/driver.py
+++ src/driver.py
@@ -178,34 +178,34 @@ def search_songs_not_in_playlist(sp, playlist_id, csv_file_path):
 
 
 # TODO: move this part to a separate class and file
-def search_filename(sp, file_name):
-    """
-    Search for a song on Spotify using the file name.
-
-    :param sp: an authenticated Spotify object
-    :param file_name: a string representing the file's name
-    :precondition: file_name is a valid string
-    :return: a string representing the track's id
-    """
-    parts = parse_filename(file_name)
-    best_match = None
-    max_popularity = -1
-
-    # Try all combinations of parts as artist and title
-    for i in range(1, len(parts)):
-        artist = ' '.join(parts[:i])
-        title = ' '.join(parts[i:])
-        query = f'track:{title} artist:{artist}'
-        result = sp.search(query, type='track', limit=1)
-        tracks = result['tracks']['items']
-        if tracks:
-            track = tracks[0]
-            # Select the track with the highest popularity
-            if track['popularity'] > max_popularity:
-                best_match = track['id']
-                max_popularity = track['popularity']
-
-    return best_match
+# def search_filename(sp, file_name):
+#     """
+#     Search for a song on Spotify using the file name.
+#
+#     :param sp: an authenticated Spotify object
+#     :param file_name: a string representing the file's name
+#     :precondition: file_name is a valid string
+#     :return: a string representing the track's id0
+#     """
+#     parts = parse_filename(file_name)
+#     best_match = None
+#     max_popularity = -1
+#
+#     # Try all combinations of parts as artist and title
+#     for i in range(1, len(parts)):
+#         artist = ' '.join(parts[:i])
+#         title = ' '.join(parts[i:])
+#         query = f'track:{title} artist:{artist}'
+#         result = sp.search(query, type='track', limit=1)
+#         tracks = result['tracks']['items']
+#         if tracks:
+#             track = tracks[0]
+#             # Select the track with the highest popularity
+#             if track['popularity'] > max_popularity:
+#                 best_match = track['id']
+#                 max_popularity = track['popularity']
+#
+#     return best_match
 
 
 def add_songs_to_playlist(sp, playlist_id, track_ids):
