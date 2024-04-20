cd37c11af1305c015ca646c6879ff9cb60779378
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2024-04-20 14:58:13 -0700

    Create flask_app.py

---
+++ src/flask_app.py
@@ -0,0 +1,11 @@
+"""
+This is the backend app, built using flask.
+"""
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+# set up a basic flask backend API
+@app.route('/api', methods=['POST'])
+def api():
+    data = request.get_json()
+    return jsonify(data)
