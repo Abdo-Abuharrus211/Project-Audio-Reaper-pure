231477941157081d2fe347f9d3003e5838cb1633
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2024-05-10 16:35:40 -0700

    Switch to backend auth, xchange code for token

--- src/driver.py
+++ src/driver.py
@@ -13,8 +13,6 @@ from fileIO import select_folder, media_file_finder, metadata_harvester, failed_
 from spotify_api_handler import get_or_create_playlist, search_songs_not_in_playlist, add_songs_to_playlist
 
 
-# TODO: return the stuff that needs to be conveyed(Songs added, songs failed...etc)
-
 class Driver:
     def __init__(self):
         self.sp = None
@@ -23,33 +21,37 @@ class Driver:
         self.added = None
         self.failed = None
 
-    def login_user(self, token):
+    def instantiate_sp_object(self, auth_code):
         load_dotenv()
         client_id = os.getenv('SPOTIFY_CLIENT_ID')
         client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
         redirect_uri = 'http://localhost:8888'
+        try:
+            sp_oauth = SpotifyOAuth(client_id, client_secret, redirect_uri,
+                                    scope='playlist-modify-public playlist-modify-private playlist-read-private')
+            auth_token = sp_oauth.get_access_token(code=auth_code)
+            self.sp = spotipy.Spotify(auth=auth_token)
 
-        # Authenticate with Spotify API and instantiate a Spotify object
-        # self.sp = spotipy.Spotify(
-        #     auth_manager=SpotifyOAuth(client_id, client_secret, redirect_uri,
-        #                               scope='playlist-modify-public playlist-modify-private playlist-read-private',
-        #                               open_browser=True))
-
-        # instantiate
-        self.sp = spotipy.Spotify(auth=token)
-        return self.sp
+            return self.sp
+        except Exception as e:
+            print(f"Error occurred while instantiating Spotify object: {e}")
+            return None
 
     def harvest(self, metadata):
-        metadata, files_without_metadata = organize_harvest(metadata)
-        filename_data = process_response(invoke_prompt_to_ai(files_without_metadata))
-        metadata.extend(filename_data)
-        playlist_id = get_or_create_playlist(self.sp, self.sp.current_user()['id'], self.playlist_name)
-        new_songs, failed_tracks = search_songs_not_in_playlist(self.sp, playlist_id, metadata)
-        # failed_csv_writer(failed_tracks)
-        added_tracks = add_songs_to_playlist(self.sp, playlist_id, new_songs)
-        self.added = added_tracks
-        self.failed = failed_tracks
-        return self.added, self.failed
+        try:
+            metadata, files_without_metadata = organize_harvest(metadata)
+            filename_data = process_response(invoke_prompt_to_ai(files_without_metadata))
+            metadata.extend(filename_data)
+            playlist_id = get_or_create_playlist(self.sp, self.sp.current_user()['id'], self.playlist_name)
+            new_songs, failed_tracks = search_songs_not_in_playlist(self.sp, playlist_id, metadata)
+            # failed_csv_writer(failed_tracks)
+            added_tracks = add_songs_to_playlist(self.sp, playlist_id, new_songs)
+            self.added = added_tracks
+            self.failed = failed_tracks
+            return self.added, self.failed
+        except Exception as e:
+            print(f"Error occurred while harvesting: {e}")
+            return None, None
 
     def set_playlist_name(self, name):
         self.playlist_name = name
