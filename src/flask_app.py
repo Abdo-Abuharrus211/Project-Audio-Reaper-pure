0dabcc956f6c8cc6bb1198040fe00a173309319f
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2024-08-11 14:52:31 -0700

    Revert SAMESITE set to 'Lax'

--- src/flask_app.py
+++ src/flask_app.py
@@ -23,11 +23,10 @@ app.config['SESSION_PERMANENT'] = False
 redis_client = redis.from_url(os.getenv('REDIS_HOST'))
 app.config['SESSION_REDIS'] = redis_client
 
-# TODO: Determine if these configs are needed
 app.config['SESSION_COOKIE_SECURE'] = True  # Set to True if using HTTPS
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_USE_SIGNER'] = True
-app.config['SESSION_COOKIE_SAMESITE'] = 'None'  # Ensure the cookie is sent with cross-site requests
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'  # Ensure the cookie is sent with cross-site requests
 
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=120)
 
